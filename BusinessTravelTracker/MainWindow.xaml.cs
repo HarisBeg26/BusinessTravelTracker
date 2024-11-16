c8bf3b7f08b35270c4257de4e16106b1b245b815
HarisBeg26 <univerzing.begovic25@gmail.com>
2024-11-16 22:30:13 +0100

    Added click event-handler for redirection to pages TripListWindow and ExpenseListWindow

--- BusinessTravelTracker/MainWindow.xaml.cs
+++ BusinessTravelTracker/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using BusinessTravelTracker.Interfaces;
+using BusinessTravelTracker.Views.ExpenseWindows;
+using BusinessTravelTracker.Views.TripWindows;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +20,29 @@ namespace BusinessTravelTracker
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly IExpenseService _expenseService;
+        private readonly ITripsService _tripsService;
+        private readonly IServiceProvider _serviceProvider;
+        public MainWindow(IServiceProvider serviceProvider, ITripsService tripsService, IExpenseService expenseService)
         {
             InitializeComponent();
+            _serviceProvider = serviceProvider;
+            _tripsService = tripsService;
+            _expenseService = expenseService;
+        }
+
+        private void btn_RedirectTravel_Click(object sender, RoutedEventArgs e)
+        {
+            var travelList = _serviceProvider.GetRequiredService<TripListWindow>();
+            travelList.Show();
+            this.Hide();
+        }
+
+        private void btn_RedirectExpense_Click(object sender, RoutedEventArgs e)
+        {
+            var expenseList = _serviceProvider.GetRequiredService<ExpenseListWindow>();
+            expenseList.Show();
+            this.Hide();
         }
     }
 }
