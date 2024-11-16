069038325ca62038e7f60421f53ec3a768d38c5a
HarisBeg26 <univerzing.begovic25@gmail.com>
2024-11-16 19:40:27 +0100

    Created a trip model layout for mapping in Supabase

--- BusinessTravelTracker/Models/Trips.cs
+++ BusinessTravelTracker/Models/Trips.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Supabase;
+using Supabase.Postgrest.Attributes;
 
 namespace BusinessTravelTracker.Models
 {
-    internal class Trips
+    [Table("trips")]
+    public class Trips :SupabaseModel
     {
+        [PrimaryKey("id", false)] // Primary key column
+        public int Id { get; set; }
+
+        [Column("destination")]
+        public string Destination { get; set; }
+
+        [Column("start_date")]
+        public DateTime StartDate { get; set; }
+
+        [Column("end_date")]
+        public DateTime EndDate { get; set; }
+
+        [Column("purpose")]
+        public string Purpose { get; set; }
     }
 }
