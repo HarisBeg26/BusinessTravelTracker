cf606b06d7da45a29642fe10d8b5a185430db8dc
HarisBeg26 <univerzing.begovic25@gmail.com>
2024-11-16 18:48:23 +0100

    Initial commit

---
+++ BusinessTravelTracker/Models/Trips.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessTravelTracker.Models
+{
+    internal class Trips
+    {
+    }
+}
